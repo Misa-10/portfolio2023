bdd68cbcec8dcff7841971ef972e29ada736f585
Misa <q.brenas@gmail.com>
2023-11-07 22:27:47 +0100

    setup project

---
+++ portfolio/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        text: '#fbfaff',
+        background: '#05010e',
+        primary: '#b7a3f5',
+        secondary: '#0c0425',
+        accent: '#4917de',
+      },
+    },
+  },
+  plugins: [],
+};
